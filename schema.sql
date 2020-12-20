68d9d0459fbe2bbb723ec0779d922ca696aa46a6
James Baldwin <jamescbaldwin@gmail.com>
2020-12-20 11:26:29 -0500

    changing heroku config

--- schema.sql
+++ schema.sql
@@ -6,20 +6,20 @@ CREATE TABLE sellers(
 id INT NOT NULL AUTO_INCREMENT,
 seller_name VARCHAR(50) NOT NULL,
 seller_item VARCHAR(50) NOT NULL,
-seller_rating DECIMAL(2,1) NOT NULL,
+-- seller_rating DECIMAL(2,1) NOT NULL,
 item_price INT(50) NOT NULL,
 date TIMESTAMP,
 PRIMARY KEY (id)
 );
 
-CREATE TABLE buyers(
-id INT NOT NULL AUTO_INCREMENT,
-buyer_name VARCHAR(50) NOT NULL,
-buyer_budget INT(50) NOT NULL,
-purchased BOOLEAN default false,
-date TIMESTAMP,
-PRIMARY KEY (id)
-);
+-- CREATE TABLE buyers(
+-- id INT NOT NULL AUTO_INCREMENT,
+-- buyer_name VARCHAR(50) NOT NULL,
+-- buyer_budget INT(50) NOT NULL,
+-- purchased BOOLEAN default false,
+-- date TIMESTAMP,
+-- PRIMARY KEY (id)
+-- );
 
 CREATE TABLE items(
 id INT NOT NULL AUTO_INCREMENT,
