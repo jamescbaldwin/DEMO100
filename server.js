e822ad722ef3dbf8668f30841d402dda78213170
James Baldwin <jamescbaldwin@gmail.com>
2021-01-07 19:45:22 -0500

    compression npm

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const passport = require("./config/passport");
+const compression = require("compression");
 
 require("dotenv").config();
 
@@ -8,6 +9,16 @@ const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 const app = express();
+app.use(compression({ filter: shouldCompress }))
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
